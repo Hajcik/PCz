85e1357efe94df58fdd4bf97fad5e02f77d76b3b
Hajcik <48524143+Hajcik@users.noreply.github.com>
2019-05-07 22:02:09 +0200

    Update Program.cs

--- Semestr 4/Programowanie Obiektowe/ConsoleApp4/ConsoleApp4/Program.cs
+++ Semestr 4/Programowanie Obiektowe/ConsoleApp4/ConsoleApp4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,6 +8,7 @@ namespace ConsoleApp4
     {
         void WypiszInfo();
     }
+
     class Osoba : IInfo
     {
         protected string imie;
@@ -25,7 +26,7 @@ namespace ConsoleApp4
         }
         public virtual void WypiszInfo()
         {
-            Console.WriteLine("Osoba: " + imie + " " + nazwisko + " " + dataUrodzenia + " ");
+            Console.WriteLine("Osoba:\n IM: " + imie + " NAZ: " + nazwisko + " DAT: " + dataUrodzenia + " ");
         }
     }
 
@@ -43,15 +44,14 @@ namespace ConsoleApp4
         }
         public override void WypiszInfo()
         {
-            Console.WriteLine("Wykladowca: " + imie + " "
-                + nazwisko + " "
-                + dataUrodzenia + " "
-                + tytulNaukowy + " "
-                + stanowisko);
+            Console.WriteLine("Wykladowca:\n IM: " + imie + " NAZ: " + nazwisko + " DAT: "
+                + dataUrodzenia + " TYT: " + tytulNaukowy + " ST: " + stanowisko);
         }
     }
+
     class Student : Osoba, IInfo
     {
+        List<OcenaKoncowa> ocenyKoncowe = new List<OcenaKoncowa>();
         private string kierunek;
         private string specjalnosc;
         private int rok;
@@ -73,62 +73,187 @@ namespace ConsoleApp4
         }
         public override void WypiszInfo()
         {
-            Console.WriteLine("Student: " + imie + " " + nazwisko + " " + dataUrodzenia + " "
-                + kierunek + " " + specjalnosc + " " + rok + " " + grupa + " " + nrIndeksu);
+            Console.WriteLine("Student:\n IM: " + imie + " NAZ: " + nazwisko + " DAT: " + dataUrodzenia + " KIER: "
+                + kierunek + " S:" + specjalnosc + " R: " + rok + " GR: " + grupa + " INDEX: " + nrIndeksu);
+        }
+        public bool DodajOcene(Przedmiot przedmiot, double ocena, string data)
+        {
+            ocenyKoncowe.Add(new OcenaKoncowa(ocena, data, przedmiot));
+            return true;
         }
         public void InfoOceny()
         {
-
+            for(int i=0; i<ocenyKoncowe.Count; i++)
+            {
+                ocenyKoncowe[i].WypiszInfo();
+            }
         }
     }
+
     class Wydzial
     {
+        List<Student> studenci = new List<Student>();
+        List<Przedmiot> przedmioty = new List<Przedmiot>();
+        List<Jednostka> jednostki = new List<Jednostka>();
+
         public void DodajJednostke(string nazwa, string adres)
         {
-
+            jednostki.Add(new Jednostka(nazwa, adres));
         }
-        public void DodajPrzedmiot(Przedmiot p)
+        public void DodajPrzedmiot(Przedmiot przedmiot)
         {
-
+            przedmioty.Add(przedmiot);
         }
-        public void DodajStudenta(Student s)
+        public void DodajStudenta(Student student)
         {
-
+            studenci.Add(student);
         }
-        public void DodajWykladowce(Wykladowca w)
+        public bool UsunStudenta(int Index_number)
         {
-
+            for (int i = 0; i < studenci.Count; i++)
+            {
+                if (studenci[i].NrIndeksu == Index_number)
+                {
+                    studenci.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void DodajWykladowce(Wykladowca wykladowca, string jednostka)
+        {
+            for (int i = 0; i < jednostki.Count; i++)
+            {
+                if(jednostki[i].Nazwa == jednostka)
+                {
+                    jednostki[i].DodajWykladowce(wykladowca);
+                }
+            }
+        }
+        public bool PrzeniesWykladowce(Wykladowca w, string obecnaJednostka, string nowaJednostka)
+        {
+            for(int i=0; i<jednostki.Count; i++)
+            {
+                if(jednostki[i].Nazwa == obecnaJednostka)
+                {
+                    jednostki[i].UsunWykladowce(w);
+                }
+            }
+            for(int i=0; i<jednostki.Count; i++)
+            {
+                if (jednostki[i].Nazwa == nowaJednostka)
+                {
+                    jednostki[i].DodajWykladowce(w);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool DodajOcene(int numer_indeksu, double ocena, Przedmiot przedmiot, string data)
+        {
+            for (int i = 0; i < studenci.Count; i++)
+            {
+                if(numer_indeksu == studenci[i].NrIndeksu)
+                {
+                    studenci[i].DodajOcene(przedmiot, ocena, data);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void InfoPrzedmioty()
+        {
+            for (int i = 0; i < przedmioty.Count; i++)
+                przedmioty[i].WypiszInfo();
+        }
+        public void InfoStudenci(bool infoOceny)
+        {       //       studenci.ForEach(i => Console.WriteLine("{0}", i));
+            if (studenci.Count == 0)
+                Console.WriteLine("Brak studentow na tym wydziale, przepraszamy.");
+            else
+            {
+                if(infoOceny == false)
+                {
+                    for (int i=0; i<studenci.Count; i++)
+                        studenci[i].WypiszInfo();
+                }
+            else if(infoOceny == true)
+                {
+                    for(int i=0; i<studenci.Count; i++)
+                    {
+                        studenci[i].WypiszInfo();
+                        studenci[i].InfoOceny();
+                    }
+                }
+            }
+        }
+        public void InfoJednostki(bool infoWykladowcy)
+        {       //       jednostki.ForEach(i => Console.WriteLine("{0}", i));
+            if (jednostki.Count == 0)
+                Console.WriteLine("Brak jednostek na tym wydziale, przepraszamy.");
+            else
+            {
+                if (infoWykladowcy == false)
+                {
+                    for(int i=0; i<jednostki.Count; i++)
+                        jednostki[i].WypiszInfo();
+                }
+            else if (infoWykladowcy == true)
+                {
+                    for(int i=0; i<jednostki.Count; i++)
+                        jednostki[i].InfoWykladowcy();
+                }
+            }
         }
     }
     class Jednostka : IInfo
     {
+        List<Wykladowca> wykladowcy = new List<Wykladowca>();
         private string nazwa;
         private string adres;
         public string Nazwa { get { return nazwa; } set { nazwa = value; } }
         public string Adres { get { return adres; } set { adres = value; } }
-        public Jednostka() { nazwa = ""; adres = ""; }
+        public Jednostka() { nazwa = ""; adres = ""; wykladowcy = null; }
         public Jednostka(string naz, string ad) { this.nazwa = naz; this.adres = ad; }
-        public void DodajWykladowce(Wykladowca wyk)
-        {
 
-        }
-        public bool UsunWykladowce(Wykladowca wyk) // po referencji
+        public void DodajWykladowce(Wykladowca wykladowca)
         {
+            wykladowcy.Add(wykladowca);
+        }
+
+        public bool UsunWykladowce(Wykladowca wykladowca) // po referencji
+        {
+            wykladowcy.Remove(wykladowca);
             return true;
         }
+
         public bool UsunWykladowce(string imie, string nazwisko) // po imieniu i nazwisku
         {
+            for (int i = 0; i < wykladowcy.Count; i++)
+            {
+                if (imie == wykladowcy[i].Imie && nazwisko == wykladowcy[i].Nazwisko)
+                {
+                    wykladowcy.RemoveAt(i);
+                }
+            }
             return true;
         }
-        public void InfoWykladowca()
-        {
 
+        public void InfoWykladowcy()
+        {
+            for (int i = 0; i < wykladowcy.Count; i++)
+            {
+                wykladowcy[i].WypiszInfo();
+            }
         }
+
         public void WypiszInfo()
         {
-
+            Console.WriteLine(nazwa + " " + adres + " ");
+            InfoWykladowcy();
         }
     }
+
     public class Przedmiot : IInfo
     {
         private string nazwa;
@@ -153,10 +278,11 @@ namespace ConsoleApp4
 
         public void WypiszInfo()
         {
-            Console.WriteLine("Przedmiot: " + nazwa + " " + kierunek + " " + specjalnosc
-                + " " + semestr + " " + ileGodzin);
+            Console.WriteLine("Przedmiot:\n " + nazwa + " K: " + kierunek + " SP: " + specjalnosc
+                + " SEM: " + semestr + " H: " + ileGodzin);
         }
     }
+
     public class OcenaKoncowa : IInfo
     {
         private double wartosc;
@@ -165,7 +291,7 @@ namespace ConsoleApp4
         public double Wartosc { get { return wartosc; } set { wartosc = value; } }
         public string Data { get { return data; } set { data = value; } }
         public Przedmiot _Przedmiot { get { return przedmiot; } set { przedmiot = value; } }
-        public OcenaKoncowa() { }
+        public OcenaKoncowa() { wartosc = 0.0; data = ""; przedmiot = null; }
         public OcenaKoncowa(double oc, string dat, Przedmiot p)
         {
             this.wartosc = oc;
@@ -174,14 +300,70 @@ namespace ConsoleApp4
         }
         public void WypiszInfo()
         {
-            Console.WriteLine("Ocena koncowa: " + przedmiot + " " + wartosc + " " + data);
+            Console.WriteLine("Ocena koncowa:\n P: " + przedmiot.Nazwa + " O: " + wartosc + " D: " + data);
         }
     }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Student student1 = new Student("Kamil", "Michalski", "15.05.1998", "INFORMATYKA", "IO", 3, 2, 1599);
+            Student student2 = new Student("Michał", "Gorzelnicki", "30.04.1996", "INFORMATYKA", "IO", 3, 2, 2232);
+            Wydzial wydzial = new Wydzial();
+            Przedmiot informatyka = new Przedmiot("Informatyka", "INFORMATYCZNY", "IO", 3, 2);
+            Przedmiot angielski = new Przedmiot("Język angielski", "HUMANISTYCZNY", "IO", 3, 3);
+            Przedmiot chinski = new Przedmiot("Język chiński", "HUMANISTYCZNY", "FILOLOGIA CHIŃSKA", 4, 5);
+            Wykladowca wykladowca1 = new Wykladowca("Adam", "Romanowski", "02.02.1976", "Profesor nadzwyczajny", "Dyrektor");
+            Wykladowca wykladowca2 = new Wykladowca("Robert", "Michalski", "19.09.1993", "Magister inżynier", "adiunkt");
+            Jednostka jed1 = new Jednostka("Politechnika", "Dabrowskiego");
+            Jednostka jed2 = new Jednostka("Uniwersytet", "Wodzickiego");
+            student1.WypiszInfo();
+            
+            student1.DodajOcene(chinski, 3.5, "02.03.2019");
+            student1.InfoOceny();
+            Console.WriteLine();
+            wykladowca1.WypiszInfo();
+            Console.WriteLine();
+            jed1.DodajWykladowce(wykladowca1);
+            jed2.DodajWykladowce(wykladowca1);
+            jed2.DodajWykladowce(wykladowca2);
+            jed1.WypiszInfo();
+            Console.WriteLine();
+            jed2.WypiszInfo();
+            Console.WriteLine();
+            jed1.InfoWykladowcy();
+            Console.WriteLine();
+            jed2.InfoWykladowcy();
+            Console.WriteLine();
+
+            wydzial.DodajJednostke("Politechnika", "Dabrowskiego");
+            wydzial.DodajPrzedmiot(informatyka);
+            wydzial.DodajPrzedmiot(angielski);
+            wydzial.DodajStudenta(student1);
+            wydzial.DodajStudenta(student2);
+            wydzial.DodajWykladowce(wykladowca1, "Politechnika");
+            wydzial.DodajWykladowce(wykladowca2, "Politechnika");
+            wydzial.InfoStudenci(false);
+            Console.WriteLine();
+            wydzial.InfoJednostki(true);
+            Console.WriteLine();
+            wydzial.InfoJednostki(false);
+            Console.WriteLine();
+            wydzial.InfoPrzedmioty();
+            Console.WriteLine();
+            wydzial.DodajOcene(1599, 5.0, informatyka, "21.03.2019");
+            wydzial.DodajOcene(2232, 4.5, angielski, "22.03.2019");
+            wydzial.InfoStudenci(true);
+            Console.WriteLine();
+            wydzial.UsunStudenta(1599);
+            wydzial.InfoStudenci(true);
+            Console.WriteLine();
+            wydzial.DodajJednostke("Najlepsza", "Jednocha");
+            wydzial.PrzeniesWykladowce(wykladowca1, "Politechnika", "Jednocha");
+            wydzial.InfoJednostki(false);
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
