55947e7b76b8be264a8950bf986bdd2e96b0db1b
Hajcik <48524143+Hajcik@users.noreply.github.com>
2019-05-07 20:59:43 +0200

    Update lab7.cs

--- Semestr 4/Programowanie Obiektowe/lab7.cs
+++ Semestr 4/Programowanie Obiektowe/lab7.cs
@@ -6,25 +6,36 @@ namespace ConsoleApp5
     {
         List<Klatka> klatki_zoo = new List<Klatka>();
         List<Opiekun> opiekunowie = new List<Opiekun>();
-        public void NowaKlatka(Klatka klatka)
+        public void DodajKlatke(Klatka klatka, Opiekun opiekun)
         {
             klatki_zoo.Add(klatka);
+            opiekun.DodajKlatke(klatka);
         }
-        public void PowiekszKlatke()
+        public void PowiekszKlatke(Klatka klatka, int pojemnosc)
         {
-
+            klatka.Pojemnosc = pojemnosc;
         }
         public void NowyOpiekun(Opiekun opiekun)
         {
             opiekunowie.Add(opiekun);
         }
-        public void NoweZwierze()
+        public void UmiescZwierze(Zwierze zwierze, Klatka klatka)
         {
-             
+            klatka.DodajZwierze(zwierze);
         }
-        public void WypiszInfo()
+        public void WypiszInfoKlatek()
         {
-
+            if(klatki_zoo.Count != 0)
+            {
+                for(int i = 0; i<klatki_zoo.Count; i++)
+                {
+                    klatki_zoo[i].WypiszInfo();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Brak klatek");
+            }
         }
     }
 
@@ -35,11 +46,25 @@ namespace ConsoleApp5
         private string nazwisko;
         public string Imie { get { return imie; } set { imie = value; } }
         public string Nazwisko { get { return nazwisko; } set { nazwisko = value; } }
+        public Opiekun() { imie = ""; nazwisko = ""; }
+        public Opiekun(string im, string naz) { this.imie = im; this.nazwisko = naz; }
         public void PosprzatajKlatki()
         {
-            for(int i = 0; i<klatki_op.Count; i++)
+            for (int i = 0; i < klatki_op.Count; i++)
             {
                 klatki_op[i].PosprzatajKlatke();
+            }
+        }
+        public void DodajKlatke(Klatka klatka)
+        {
+            klatki_op.Add(klatka);
+        }
+        public void WypiszInfo()
+        {
+            Console.WriteLine("Nazwisko: " + Nazwisko + "Przydzielone klatki: ");
+            for (int i = 0; i < klatki_op.Count; i++)
+            {
+                Console.WriteLine("ID: " + klatki_op[i].ID_Klatki);
             }
         }
     }
@@ -54,7 +79,28 @@ namespace ConsoleApp5
         public Klatka(int poj, int id) { this.pojemnosc = poj; this.id_klatki = id; }
         public void PosprzatajKlatke()
         {
-            Console.WriteLine()
+            Console.WriteLine("Posprzatana zostala klatka o numerze ID: " + id_klatki);
+        }
+        public bool DodajZwierze(Zwierze zwierze)
+        {
+            if(zwierzeta.Count < pojemnosc)
+            {
+                zwierzeta.Add(zwierze);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Pelna klatka");
+                return false;
+            }
+        }
+        public void WypiszInfo()
+        {
+            Console.WriteLine("ID klatki: " + ID_Klatki + " Pojemnosc: " + Pojemnosc);
+            for(int i = 0; i<zwierzeta.Count; i++)
+            {
+                zwierzeta[i].WypiszInfo();
+            }
         }
     }
     abstract class Zwierze
@@ -66,17 +112,23 @@ namespace ConsoleApp5
         public string Pozywienie { get { return pozywienie; } set { pozywienie = value; } }
         public string Pochodzenie { get { return pochodzenie; } set { pochodzenie = value; } }
         public Zwierze()
-        {   gatunek = ""; pozywienie = ""; pochodzenie = "";    }
+        { gatunek = ""; pozywienie = ""; pochodzenie = ""; }
         public Zwierze(string gat, string poz, string poc)
-        {   this.gatunek = gat; this.pozywienie = poz; this.pochodzenie = poc;   }
+        { this.gatunek = gat; this.pozywienie = poz; this.pochodzenie = poc; }
+        public abstract void WypiszInfo();
     }
 
     class Ssak : Zwierze
     {
         private string srodowisko;
         public string Srodowisko { get { return srodowisko; } set { srodowisko = value; } }
-        public Ssak() :base() { srodowisko = ""; }
-        public Ssak(string gat, string poz, string poc, string srod) :base(gat, poz, poc) { this.srodowisko = srod; }
+        public Ssak() : base() { srodowisko = ""; }
+        public Ssak(string gat, string poz, string poc, string srod) : base(gat, poz, poc) { this.srodowisko = srod; }
+        public override void WypiszInfo()
+        {
+            Console.WriteLine("Gatunek: " + Gatunek + " " + " Pozywienie: " + Pozywienie +
+               " Pochodzenie: " + Pochodzenie + " Srodowisko: " + Srodowisko);
+        }
     }
 
     class Ptak : Zwierze
@@ -87,18 +139,26 @@ namespace ConsoleApp5
         public float Rozpietosc { get { return rozpietosc; } set { rozpietosc = value; } }
         public float Wytrzymalosc { get { return wytrzymalosc; } set { wytrzymalosc = value; } }
         public string Zdolnosc { get { return zdolnosc; } set { zdolnosc = value; } }
-        public Ptak() :base() { rozpietosc = 0.0f; wytrzymalosc = 0.0f; }
-        public Ptak(string gat, string poz, string poc, float roz, float wyt, string zd) :base(gat, poz, poc)
-        {   this.rozpietosc = roz; this.wytrzymalosc = wyt; this.zdolnosc = zd; }
-        public void MaxDlugoscLotu()
-        {   Console.WriteLine("Ten ptak " + Gatunek + "moze leciec najwiecej " + rozpietosc*wytrzymalosc + "[j]"); }
+        public Ptak() : base() { rozpietosc = 0.0f; wytrzymalosc = 0.0f; zdolnosc = "none"; }
+        public Ptak(string gat, string poz, string poc, float roz, float wyt, string zd) : base(gat, poz, poc)
+        { this.rozpietosc = roz; this.wytrzymalosc = wyt; this.zdolnosc = zd; }
+        public double MaxDlugoscLotu(double rozpietosc, double wytrzymalosc)
+        { 
+            return rozpietosc* wytrzymalosc;
+        }
+        public override void WypiszInfo()
+        {
+            Console.WriteLine("Gatunek: " + Gatunek + " " + " Pozywienie: " + Pozywienie + " Pochodzenie: " + Pochodzenie
+                + " Rozpietosc: " + Rozpietosc + " Wytrzymalosc: " + Wytrzymalosc);
+            Console.WriteLine("Dlugosc lotu: " + MaxDlugoscLotu(rozpietosc, wytrzymalosc));
+        }
     }
     class Gad : Zwierze
     {
         private bool jadowitosc;
         public bool Jadowitosc { get { return jadowitosc; } set { jadowitosc = value; } }
-        public Gad() :base() { jadowitosc = false; }
-        public Gad(string gat, string poz, string poc, bool jad) :base (gat, poz, poc)
+        public Gad() : base() { jadowitosc = false; }
+        public Gad(string gat, string poz, string poc, bool jad) : base(gat, poz, poc)
         { this.jadowitosc = jad; }
 
         public bool CzyGadJadowity()
@@ -107,11 +167,42 @@ namespace ConsoleApp5
             { return true; }
             else return false;
         }
+        public override void WypiszInfo()
+        {
+            Console.WriteLine("Gatunek: " + Gatunek + " " + " Pozywienie: " + Pozywienie +
+                " Pochodzenie: " + Pochodzenie + " Jadowity: " + Jadowitosc);
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
+
+            Opiekun Artur = new Opiekun("ARTUR","MIRKOWSKI");
+            Opiekun Krystian = new Opiekun("KRYSTIAN", "ZABOLAŁO");
+            Zwierze orzel = new Ptak("PTAK", "ROBAKI", "POLSKA",  10.2f, 2.3f, "SZYBKI CHWYT");
+            Zwierze waz = new Gad("GAD", "KRÓLIK", "AMERYKA", true);
+            Zwierze koala = new Ssak("SSAK", "EUKALIPTUS", "AFRYKA", "DRZEWO");
+            Klatka klatka1 = new Klatka(5, 1);
+            Klatka klatka2 = new Klatka(1, 2);
+            Zoo zoo = new Zoo();
+            zoo.WypiszInfoKlatek();
+            zoo.NowyOpiekun(Artur);
+            zoo.NowyOpiekun(Krystian);
+            zoo.DodajKlatke(klatka1, Artur);
+            zoo.DodajKlatke(klatka2, Krystian);
+            zoo.UmiescZwierze(orzel, klatka1);
+            zoo.UmiescZwierze(waz, klatka2);
+            zoo.UmiescZwierze(koala, klatka1);
+            zoo.WypiszInfoKlatek();
+            Console.WriteLine();
+            zoo.PowiekszKlatke(klatka2, 2);
+            zoo.WypiszInfoKlatek();
+            Krystian.PosprzatajKlatki();
+            Artur.PosprzatajKlatki();
+
+
+            Console.ReadKey();
             Console.WriteLine("Hello World!");
         }
     }
