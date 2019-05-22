7e3323d6a828686756f043ea33ce71a348ffe287
Hajcik <48524143+Hajcik@users.noreply.github.com>
2019-05-22 20:46:16 +0200

    Update mv.sh

--- Semestr 4/Systemy Operacyjne/Linux/mv.sh
+++ Semestr 4/Systemy Operacyjne/Linux/mv.sh
@@ -1,21 +1,22 @@
+#!/bin/bash
 echo "Chcesz zmienić nazwę pliku (1) czy go przenieść (2)?"
-read wybor
+read -r wybor
 case $wybor in
   "1")echo "ZMIANA NAZWY PLIKU: "
       echo "Wprowadz nazwę pliku: "
-          read nazwa1
+          read -r nazwa1
       echo "Wprowadzona nowa nazwa pliku: "
-          read nazwa2
-            touch $nazwa1
-            touch $nazwa2
-            cp $nazwa1 $nazwa2
-            rm $nazwa1 ;;
+          read -r nazwa2
+            touch "$nazwa1"
+            touch "$nazwa2"
+            cp "$nazwa1" "$nazwa2"
+            rm "$nazwa1" ;;
    "2")echo "PRZENIESIENIU PLIKU: "
-          read plik
-       touch $plik
+          read -r plik
+       touch "$plik"
        echo "Wprowadz nazwę folderu gdzie chcesz przenieść plik: "
-          read folder
-       cp $plik $folder
-       rm $plik ;;
+          read -r folder
+       cp "$plik" "$folder"
+       rm "$plik" ;;
    *)echo "WPROWADZONO ZŁĄ WARTOŚĆ!"
 esac
