9bed283e2a5bdf88706293dd363f9a2d3d1b7a4d
Hajcik <48524143+Hajcik@users.noreply.github.com>
2019-05-31 11:41:27 +0200

    Update DLA_WAS_CHLOPAKI.cs

--- Semestr 4/Paradygmaty Programowania/DLA_WAS_CHLOPAKI.cs
+++ Semestr 4/Paradygmaty Programowania/DLA_WAS_CHLOPAKI.cs
@@ -20,28 +20,51 @@ namespace ConsoleApp27
             }
             public override string ToString()
             {
-                return LastName + " " + FirstName + " " + Age + "\n";
+                return LastName + " " + FirstName + " " + Age;
             }
         }
         static void Main(string[] args)
         {
             List<Person> Ludzie = new List<Person>();
             Person osoba1 = new Person("Michal", "Wisniewski", 30);
-            Person osoba2 = new Person("Radek", "Gorzelski", 25);
+            Person osoba2 = new Person("Mariusz", "Gorzelski", 25);
             Person osoba3 = new Person("Adam", "Wkladaj", 19);
             Person osoba4 = new Person("Mlody", "Maniek", 17);
             Person osoba5 = new Person("Janek", "Adamowski", 14);
+            Person osoba6 = new Person("Dawido", "Kmako", 13);
+            Person osoba7 = new Person("Janek", "Zawadzki", 93);
+            Person osoba8 = new Person("Bambuko", "Afryko", 2);
+            Person osoba9 = new Person("Mariusz", "Maloletni", 10);
+            Person osoba10 = new Person("Mariusz", "Wieloletni", 45);
             Ludzie.Add(osoba1);
             Ludzie.Add(osoba2);
             Ludzie.Add(osoba3);
             Ludzie.Add(osoba4);
             Ludzie.Add(osoba5);
-
+            Ludzie.Add(osoba6);
+            Ludzie.Add(osoba7);
+            Ludzie.Add(osoba8);
+            Ludzie.Add(osoba9);
+            Ludzie.Add(osoba10);
+            // podpunkt A
             var alfabetycznie = 
                 Ludzie.OrderBy(Person => Person.LastName).ToList();
             foreach (var osoba in alfabetycznie)
-            { Console.Write(osoba.ToString() );  }
+            { Console.WriteLine(osoba.ToString() );  }
+            Console.WriteLine();
+            // podpunkt B
+         
+            var test =
+                (from n in Ludzie
+                 where n.Age > 18
+                 where n.FirstName == "Mariusz"
+                 select n).ToList();
+            foreach(var osoba in test)
+            { Console.WriteLine(osoba.ToString() ); }
+
             Console.ReadKey();
+
+
         }
     }
 }
