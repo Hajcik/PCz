3b20d0e126af2810fad3fcd13bed8ac9c746337b
Hajcik <48524143+Hajcik@users.noreply.github.com>
2019-10-14 11:03:34 +0000

    Update lab1.cpp

--- Semestr 5/Programowanie Współbieżne i Rozproszone/Laboratorium 1/lab1.cpp
+++ Semestr 5/Programowanie Współbieżne i Rozproszone/Laboratorium 1/lab1.cpp
@@ -6,18 +6,25 @@ int main()
 {
 	int rank; // ID procesu
 	int size; // Liczba uruchomionych procesow
+	int len;
+	char name[MPI_MAX_PROCESSOR_NAME];	
 		
 	MPI_Init(NULL, NULL);
 	
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	
+	MPI_Get_processor_name(name, &len);
 //	cout<<"HELLO WORLD, RANK: "<<rank<<" SIZE: "<<size<<endl;
-
+	cout<<"HELLO WORLD, RANK: "<<rank<<" SIZE: "<<size
+		<<" PROCESSOR NAME: "<<name<<endl;
+	
 	int temp = 0;
 	
+	
+	//Funkcja cin jest dostepna tylko dla ranku o id=0
 	if(rank == 0)
 	{
+		// cin >> temp
 		temp = 10;
 	}
 	else
