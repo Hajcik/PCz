62ed5bd629a47a116e75d9f3f2c5f7055cdba963
Hajcik <48524143+Hajcik@users.noreply.github.com>
2019-11-20 19:30:41 +0100

    Update script.js

--- Semestr 5/Programowanie Stron Internetowych/Laboratorium/Laboratorium 4/script.js
+++ Semestr 5/Programowanie Stron Internetowych/Laboratorium/Laboratorium 4/script.js
@@ -1,5 +1,4 @@
-  
-// Generacja liczby między 1 a 10
+// Generacja liczby między 1 a 10 --- Zad1
 var y = Math.floor(Math.random() * 10 + 1); 
                 
 document.getElementById("submitguess").onclick = function(){ 
@@ -18,4 +17,40 @@ else
 { 
     alert("Spróbuj liczbę większą ;)") 
 } 
+}
+
+
+// Operacja mnożenia i dzielenia --- Zad2
+
+function mnoz()
+{
+    num1 = document.getElementById("1_liczba").value;
+    num2 = document.getElementById("2_liczba").value;
+    document.getElementById("wynik").innerHTML = num1 * num2;
+}
+
+function dziel()
+{
+    num1 = document.getElementById("1_liczba").value;
+    num2 = document.getElementById("2_liczba").value;
+    if(num2 == 0)
+    { 
+        document.getElementById("wynik").innerHTML = "Nie dzielimy przez 0"; 
+    }
+    else
+    {
+        document.getElementById("wynik").innerHTML = num1 / num2;
+    }
+}
+
+function konwersjaCnaF()
+{
+    cels = document.getElementById("celsjusz").value;
+
+    document.getElementById("wynik_konwersji").innerHTML = (cels * (9/5)) + 32;
+}
+
+function konwersjaFnaC()
+{
+    
 }
