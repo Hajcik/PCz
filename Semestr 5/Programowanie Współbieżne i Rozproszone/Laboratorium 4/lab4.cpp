f4001ca516e87572c834323da8cf72b95dd82f22
Hajcik <48524143+Hajcik@users.noreply.github.com>
2019-12-01 13:14:16 +0100

    Update lab4.cpp

--- Semestr 5/Programowanie Współbieżne i Rozproszone/Laboratorium 4/lab4.cpp
+++ Semestr 5/Programowanie Współbieżne i Rozproszone/Laboratorium 4/lab4.cpp
@@ -1,3 +1,43 @@
+/*
+Wykorzystując bibliotekę MPI napisać równoległą wersję poniższego kodu:
+
+float max(const float &a, const float &b)
+{
+	return (a>b)?a:b;
+}
+
+int main(int argv, char **argc)
+{
+
+int n;
+std::cout<<"Wprowadz rozmiar tablicy: ";
+std::cin>>n;
+
+int *tab = new int[n];
+
+for(int i = 0; i<n; ++i)
+	tab[i] = i;
+
+int mx = tab[0];
+
+for(int i=1; i<n; ++i)
+	mx = max(mx, tab[i]);
+
+std::cout<<"max: "<<mx<<std::endl;
+delete [] tab;
+}
+
+Zakładamy, że liczba elementów wektora jest n razy większa od liczby procesów size. Główna tablica
+tworzona jest w obrębie 0 procesu. Rozmiar nRank odpowiednich fragmentów tablicy należy rozesłać do
+pozostałych procesów przy pomocy funkcji MPI_Bcast. Odpowiednie fragmenty tablicy rozsyłane są do
+pozostałych procesów przy pomocy funkcji MPI_Scatter. Każdy proces poszukuje największego elementu
+w swojej części tablicy. Przy pomocy funkcji MPI_Reduce należy znaleźć największy element spośród
+największych elementów znalezionych w poszczególnych procesach.
+MPI_Bcast (*buffer,count,datatype,root,comm)
+MPI_Scatter (*sendbuf,sendcnt,sendtype,*recvbuf, recvcnt,recvtype,root,comm)
+MPI_Reduce (*sendbuf,*recvbuf,count,datatype,op,root,comm) 
+*/
+
 #include<iostream>
 #include<mpi.h>
 #include<ctime>
